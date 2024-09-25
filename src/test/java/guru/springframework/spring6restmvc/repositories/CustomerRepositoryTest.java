3083a9cddc00411db62cec9d3749125d4d77c3c3
DawidBialek <dd.bialek@gmail.com>
2024-09-26 00:18:50 +0100

    Remove unused imports

--- src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -1,13 +1,11 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Customer;
-import org.hibernate.id.enhanced.CustomOptimizerDescriptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class CustomerRepositoryTest {
