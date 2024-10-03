c5fd2810ac354f6c68643843049610dd82965f24
DawidBialek <dd.bialek@gmail.com>
2024-10-04 00:15:38 +0100

    Add BootstrapData, mappers and BootstrapData test

---
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/bootstrapDataTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring6restmvc.bootstrap;
+
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class bootstrapDataTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    BootstrapData bootstrapData;
+
+    @BeforeEach
+    void setUp() {
+        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+    }
+
+    @Test
+    void testBootstrapData() throws Exception {
+        bootstrapData.run(null);
+
+        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(customerRepository.count()).isEqualTo(3);
+    }
+}
