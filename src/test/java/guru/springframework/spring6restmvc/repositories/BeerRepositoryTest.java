4c5cdc5876121c7756d43465b791fa82bd46eabc
DawidBialek <dd.bialek@gmail.com>
2024-10-10 22:34:39 +0100

    Update validation for Beer, add validations tests and new error handler

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,11 +1,16 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -14,10 +19,30 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 646543246546584654654684652432165476987954321654987623216546879654321657968476214658476254568468475")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("4354321354324")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
+
+    @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("4354321354324")
+                .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
